72fda4c52bfe975d228140d825d418c5d677847f
Divya <divyadhar@berkeley.edu>
2020-11-27 00:19:59 -0800

    Add: Initial Twitter Consumer - uses spark structured streaming to pull tweets from kafka - will have additions

---
+++ twitterConsumer/consumer.py
@@ -0,0 +1,43 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+from pyspark.sql import types as T
+#import boto3
+
+# Schema of Tweet and Tweet Metadata from Twitter API
+json_schema = T.StructType()
+  .add("data", T.StructType().add( "id", T.LongType ).add("created_at", ).add("text", T.StringType) )
+  .add("matching_rules", TStructType().add( "id", T.StringType ).add("tag", T.StringType) )
+
+
+spark = SparkSession \
+    .builder \
+    .appName("StructuredNetworkWordCount") \
+    .getOrCreate()
+
+# Start Spark Structured Streaming 
+df = spark \
+  .readStream \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", "34.213.179.50:9092,34.220.78.168:9092,34.210.71.28:9092") \
+  .option("subscribe", "twitter_on_stocks") \
+  .load()
+
+tweets = df.selectExpr("CAST(value) AS str")
+
+#df = df.selectExpr("data.id as id", "data.created_at as created_at", "data.text", "matching_rules.tag as company")
+
+tweets = tweets.select( f.from_json(tweets.value, json_schema).as("value")  ) 
+
+tweets = tweets.selectExpr( "value.data.id as id", "value.data.created_at as created_at", "value.data.text as text", "matching_rules.tag as company" )
+
+# Count words in the tweet
+tweets = tweets.withColumn( "word_count",  f.size(f.split(f.col('text'), ' '))  )
+
+# Classify tweets
+
+# Save set of data pulled from Kafka every 15 seconds
+df.writeStream.format("parquet"). \
+    option("checkpointLocation", "s3n://tweets1/"). \
+    outputMode('append'). \
+    trigger(processingTime='15 seconds'). \
+    partitionBy('company')

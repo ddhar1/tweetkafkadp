20858090a79b470970090790c8375db634ddfc41
Divya <divyadhar@berkeley.edu>
2020-12-28 18:43:19 -0800

    Add: logging to main Producer Method\n Added logging to main producer method to notify when connecting to twitter API and starting sending data from API to producer.\n Add try catch method to gracefully exit with non-zero status code if working with producer fails and add exception information to log

--- twitterProducer/src/main/java/FilteredStreamDemo.java
+++ twitterProducer/src/main/java/FilteredStreamDemo.java
@@ -1,6 +1,8 @@
 /*
 Main Class to access Twitter Stream API and send tweets using Producer to Kafka
  */
+import org.apache.log4j.Logger;
+
 import java.io.*;
 import java.net.URISyntaxException;
 
@@ -10,10 +12,23 @@ public class FilteredStreamDemo {
     private static final String bootstrapServers = "34.213.179.50:9092";
 
     public static void main(String args[]) throws IOException, URISyntaxException {
+        // Start logging
+        Log4JProperties logProperties = new Log4JProperties();
+        Logger log = Logger.getLogger("FilteredStreamDemo");
+
+        log.info("Connecting to twitter");
         TwitterAPIConnector twitterAPIConnector = new TwitterAPIConnector();
         twitterAPIConnector.deleteAllRules();
         twitterAPIConnector.addRulesfromFile("./companies.txt");
-        //twitterAPIConnector.sendTweetStreamToKafka( TOPIC_NAME, bootstrapServers, 2 );
+
+        log.info("Sending tweets to twitter");
+        try {
+            //twitterAPIConnector.sendTweetStreamToKafka( TOPIC_NAME, bootstrapServers, 2 );
+        }
+        catch (Exception e) {
+            log.fatal("twitterAPIConnector.sendTweetStreamToKafka failed. Please see logs", e);
+            System.exit(-1);
+        }
     }
 
 

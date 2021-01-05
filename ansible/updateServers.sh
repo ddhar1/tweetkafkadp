cf126fb77198d18e012c61b766c94a22ab3154f8
ddhar1 <divyadhar@berkeley.edu>
2021-01-05 02:02:11 -0800

    Fix: Adding line to fix hosts.yml_raw to replace latest kafka broker's ip address

--- ansible/updateServers.sh
+++ ansible/updateServers.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# replace with correct ip addresses
-zookeeper="54.149.164.41"
-broker0="34.213.179.50"
-broker1="34.220.78.168"
-broker2="34.210.71.28"
+# TODO: replace with correct ip addresses
+zookeeper="54.186.71.141"
+broker0="54.186.232.239"
+broker1="54.187.119.73"
+broker2="34.213.55.244"
 
+#sed's -e allows you to replace keywords with current IP address
 # broker 0
-#> ./ansible/server_configs/server0/server.properties
-sed -e "s/brokerserverid/${broker0}/g"  -e "s/zookeeperserverid/${zookeeper}/g" ./ansible/server_configs/server0/server.properties_raw > ./ansible/server_configs/server0/server.properties
+sed -e "s/brokerserverid/${broker0}/g"  -e "s/zookeeperserverid/${zookeeper}/g" ./server_configs/server0/server.properties_raw > ./server_configs/server0/server.properties
 # broker 1
-sed -e "s/brokerserverid/${broker1}/g" -e "s/zookeeperserverid/${zookeeper}/g" ./ansible/server_configs/server1/server.properties_raw > ./ansible/server_configs/server1/server.properties
+sed -e "s/brokerserverid/${broker1}/g" -e "s/zookeeperserverid/${zookeeper}/g" ./server_configs/server1/server.properties_raw > ./server_configs/server1/server.properties
 # broker 2
-sed -e "s/brokerserverid/${broker2}/g" -e "s/zookeeperserverid/${zookeeper}/g" ./ansible/server_configs/server2/server.properties_raw > ./ansible/server_configs/server2/server.properties
+sed -e "s/brokerserverid/${broker2}/g" -e "s/zookeeperserverid/${zookeeper}/g" ./server_configs/server2/server.properties_raw > ./server_configs/server2/server.properties
+
+sed -e "s/brokerserverid0/${broker0}/g" -e "s/brokerserverid1/${broker1}/g" -e "s/brokerserverid2/${broker2}/g" -e "s/zookeeperserverid/${zookeeper}/g" ./server_configs/hosts.yml_raw > ./hosts.yml
 
 # zookeeper
 #sed -e "s/brokerserverid/${broker2}/g" ./ansible/server_configs/server2/server.properties_raw > ./ansible/server_configs/server2/server.properties

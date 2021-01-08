3e286024300e5e03f51eb0a385d6ad0060b81c20
Divya <divyadhar@berkeley.edu>
2021-01-07 22:34:15 -0800

    Fix: Changed Kafka Producer Acks from all to 1
    ACKS_CONFIG set to 1 and ENABLE_IDEMPOTENCE_CONFIG set to false
    If I had access to twitter's actually production API as opposed to the filtered Streams API, I would likely receive a lot more streams per second. Since it's likely not important to get every single stream (just the majority of them), I would like to set ACKS required to be just from the leader broker instead of all brokers in the kafka cluster.

--- twitterProducer/src/main/java/TwitterProducer.java
+++ twitterProducer/src/main/java/TwitterProducer.java
@@ -35,8 +35,8 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // create safe Producer
-        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
-        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "false");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "1");
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5"); // kafka 2.0 >= 1.1 so we can keep this as 5. Use 1 otherwise.
 

65c2c35c387dfbbe18d13e7c5a44af8b01bb6e71
ddhar1 <divyadhar@berkeley.edu>
2021-01-12 21:22:06 -0800

    Fix: Change reference to host.yml_raw given files new location

--- ansible/updateServers.sh
+++ ansible/updateServers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # TODO: replace with correct ip addresses
-zookeeper="54.186.71.141"
-broker0="54.186.232.239"
-broker1="54.187.119.73"
-broker2="34.213.55.244"
+zookeeper="52.40.198.39"
+broker0="54.149.106.83"
+broker1="54.191.231.179"
+broker2="52.24.205.75"
 
 #sed's -e allows you to replace keywords with current IP address
 # broker 0
@@ -14,7 +14,7 @@ sed -e "s/brokerserverid/${broker1}/g" -e "s/zookeeperserverid/${zookeeper}/g" .
 # broker 2
 sed -e "s/brokerserverid/${broker2}/g" -e "s/zookeeperserverid/${zookeeper}/g" ./server_configs/server2/server.properties_raw > ./server_configs/server2/server.properties
 
-sed -e "s/brokerserverid0/${broker0}/g" -e "s/brokerserverid1/${broker1}/g" -e "s/brokerserverid2/${broker2}/g" -e "s/zookeeperserverid/${zookeeper}/g" ./server_configs/hosts.yml_raw > ./hosts.yml
+sed -e "s/brokerserverid0/${broker0}/g" -e "s/brokerserverid1/${broker1}/g" -e "s/brokerserverid2/${broker2}/g" -e "s/zookeeperserverid/${zookeeper}/g" ./hosts.yml_raw > ./hosts.yml
 
 # zookeeper
 #sed -e "s/brokerserverid/${broker2}/g" ./ansible/server_configs/server2/server.properties_raw > ./ansible/server_configs/server2/server.properties

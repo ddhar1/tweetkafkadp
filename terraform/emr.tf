b536b775c10df20d0410a3c8a66bc88ad3ab9aa7
ddhar1 <divyadhar@berkeley.edu>
2021-01-17 12:23:37 -0800

    Fix: Applications installed in EMR, Service Roles

--- terraform/emr.tf
+++ terraform/emr.tf
@@ -1,17 +1,17 @@
 resource "aws_emr_cluster" "cluster" {
     name ="emr_for_sparkstreaming"
     release_label = "emr-5.31.0"
-    applications = [ "Spark 2.4.6" ]
+    applications = [ "Spark" ]
 
     termination_protection = false
 
-    service_role = "EMR_EC2_DefaultRole"
+    service_role = "EMR_DefaultRole" #TODO: Insert role of choice
 
     ec2_attributes {
         subnet_id = aws_subnet.prod-subnet-public-1.id
-        emr_managed_master_security_group = "emr"
-        emr_managed_slave_security_group = "emr"
-        instance_profile = "EMR_EC2_DefaultRole"
+        emr_managed_master_security_group = aws_security_group.securitygroupforemr.id
+        emr_managed_slave_security_group = aws_security_group.securitygroupforemr.id
+        instance_profile = "EMR_EC2_DefaultRole" #TODO: Insert role of choice
         key_name = var.key_name
 
     }
@@ -37,14 +37,21 @@ resource "aws_emr_cluster" "cluster" {
 
 }
 
-resource "aws_security_group" "emr" {
+resource "aws_security_group" "securitygroupforemr" {
     description = "Security group for opening ports used by kafka"
     vpc_id = aws_vpc.prod-vpc.id
 
-    name = "Public Access"
+    name = "SecurityGroup4EMR"
 
-        tags = {
+    tags = {
         project = "tweetkafkadp"
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks =["0.0.0.0/0"]
     }
 
 }
